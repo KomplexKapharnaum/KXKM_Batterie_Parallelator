a9676b22d89aa02b93257699594738897d9d275a
Clément SAILLANT <clement@lelectronrare.fr>
2024-12-11 14:20:40 +0100

    SD-logger to csv

--- src/SDLogger.cpp
+++ src/SDLogger.cpp
@@ -1,27 +1,47 @@
-
 #include "SDLogger.h"
 
 SDLogger::SDLogger() {}
 
-void SDLogger::begin() {
-    if (!SD.begin(chipSelect)) {
+void SDLogger::begin()
+{
+    if (!SD.begin(chipSelect))
+    {
         Serial.println("Initialization of SD card failed!");
         return;
     }
-    dataFile = SD.open("datalog.txt", FILE_WRITE);
-    if (!dataFile) {
-        Serial.println("Failed to open datalog.txt for writing!");
+    dataFile = SD.open("datalog.csv", FILE_WRITE);
+    if (!dataFile)
+    {
+        Serial.println("Failed to open datalog.csv for writing!");
     }
 }
 
-void SDLogger::logData(int deviceNumber, float data) {
-    if (dataFile) {
-        dataFile.print("Device ");
-        dataFile.print(deviceNumber);
-        dataFile.print(": ");
-        dataFile.println(data);
+void SDLogger::logData(float volt, float current, bool switchState, unsigned long time)
+{
+    if (dataFile)
+    {
+        dataFile.print(time);
+        dataFile.print(",");
+        dataFile.print(volt);
+        dataFile.print(",");
+        dataFile.print(current);
+        dataFile.print(",");
+        dataFile.println(switchState ? "ON" : "OFF");
         dataFile.flush();
-    } else {
-        Serial.println("Error writing to datalog.txt");
     }
+    else
+    {
+        Serial.println("Error writing to datalog.csv");
+    }
+}
+
+bool SDLogger::shouldLog()
+{
+    unsigned long currentTime = millis();
+    if (currentTime - lastLogTime >= 10000)
+    { // 10 secondes
+        lastLogTime = currentTime;
+        return true;
+    }
+    return false;
 }
--- src/TCA_Func.h
+++ src/TCA_Func.h
@@ -1,6 +1,7 @@
 #include "TCA9555.h" // TCA9555 library
 
-class TCAHandler {
+class TCAHandler
+{
 public:
     TCAHandler();
     void setup();
@@ -11,7 +12,7 @@ public:
     byte getDeviceAddress(int TCA_num);
 
 private:
-    void initialize_tca(TCA9535 &tca, const char* name);
+    void initialize_tca(TCA9535 &tca, const char *name);
 
     const byte TCA_address[8] = {0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27}; // TCA9535 address array
     TCA9535 TCA_0, TCA_1, TCA_2, TCA_3, TCA_4, TCA_5, TCA_6, TCA_7;
@@ -19,47 +20,72 @@ private:
     byte TCA_address_connected[8];
 };
 
-TCAHandler::TCAHandler() 
-    : TCA_0(TCA_address[0]), TCA_1(TCA_address[1]), TCA_2(TCA_address[2]), TCA_3(TCA_address[3]), 
-      TCA_4(TCA_address[4]), TCA_5(TCA_address[5]), TCA_6(TCA_address[6]), TCA_7(TCA_address[7]), 
-      Nb_TCA(0) { // Initialisation de Nb_TCA
+TCAHandler::TCAHandler()
+    : TCA_0(TCA_address[0]), TCA_1(TCA_address[1]), TCA_2(TCA_address[2]), TCA_3(TCA_address[3]),
+      TCA_4(TCA_address[4]), TCA_5(TCA_address[5]), TCA_6(TCA_address[6]), TCA_7(TCA_address[7]),
+      Nb_TCA(0)
+{ // Initialisation de Nb_TCA
     memset(TCA_address_connected, 0, sizeof(TCA_address_connected));
 }
 
-void TCAHandler::initialize_tca(TCA9535 &tca, const char* name) {
+void TCAHandler::initialize_tca(TCA9535 &tca, const char *name)
+{
     tca.begin();
     Serial.println(String(name) + " is connected");
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
         tca.pinMode1(i, OUTPUT);
         tca.write1(i, LOW);
     }
-    for (int i = 5; i < 7; i++) {
+    for (int i = 5; i < 7; i++)
+    {
         tca.pinMode1(i, INPUT);
     }
-    for (int i = 8; i < 16; i++) {
+    for (int i = 8; i < 16; i++)
+    {
         tca.pinMode1(i, OUTPUT);
         tca.write1(i, LOW);
     }
 }
 
-void TCAHandler::setup() {
+void TCAHandler::setup()
+{
     Serial.println();
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++)
+    {
         Wire.beginTransmission(TCA_address[i]);
         delay(50);
-        if (Wire.endTransmission() == 0) {
+        if (Wire.endTransmission() == 0)
+        {
             Nb_TCA++;
-            TCA_address_connected[Nb_TCA-1] = TCA_address[i];
+            TCA_address_connected[Nb_TCA - 1] = TCA_address[i];
 
-            switch (i) {
-                case 0: initialize_tca(TCA_0, "TCA_0"); break;
-                case 1: initialize_tca(TCA_1, "TCA_1"); break;
-                case 2: initialize_tca(TCA_2, "TCA_2"); break;
-                case 3: initialize_tca(TCA_3, "TCA_3"); break;
-                case 4: initialize_tca(TCA_4, "TCA_4"); break;
-                case 5: initialize_tca(TCA_5, "TCA_5"); break;
-                case 6: initialize_tca(TCA_6, "TCA_6"); break;
-                case 7: initialize_tca(TCA_7, "TCA_7"); break;
+            switch (i)
+            {
+            case 0:
+                initialize_tca(TCA_0, "TCA_0");
+                break;
+            case 1:
+                initialize_tca(TCA_1, "TCA_1");
+                break;
+            case 2:
+                initialize_tca(TCA_2, "TCA_2");
+                break;
+            case 3:
+                initialize_tca(TCA_3, "TCA_3");
+                break;
+            case 4:
+                initialize_tca(TCA_4, "TCA_4");
+                break;
+            case 5:
+                initialize_tca(TCA_5, "TCA_5");
+                break;
+            case 6:
+                initialize_tca(TCA_6, "TCA_6");
+                break;
+            case 7:
+                initialize_tca(TCA_7, "TCA_7");
+                break;
             }
         }
     }
@@ -68,50 +94,92 @@ void TCAHandler::setup() {
     Serial.println(" devices");
 }
 
-bool TCAHandler::read(int TCA_num, int pin) {
+bool TCAHandler::read(int TCA_num, int pin)
+{
     bool val;
-    switch (TCA_num) {
-        case 0: val = TCA_0.read1(pin); break;
-        case 1: val = TCA_1.read1(pin); break;
-        case 2: val = TCA_2.read1(pin); break;
-        case 3: val = TCA_3.read1(pin); break;
-        case 4: val = TCA_4.read1(pin); break;
-        case 5: val = TCA_5.read1(pin); break;
-        case 6: val = TCA_6.read1(pin); break;
-        case 7: val = TCA_7.read1(pin); break;
-        default: return false;
+    switch (TCA_num)
+    {
+    case 0:
+        val = TCA_0.read1(pin);
+        break;
+    case 1:
+        val = TCA_1.read1(pin);
+        break;
+    case 2:
+        val = TCA_2.read1(pin);
+        break;
+    case 3:
+        val = TCA_3.read1(pin);
+        break;
+    case 4:
+        val = TCA_4.read1(pin);
+        break;
+    case 5:
+        val = TCA_5.read1(pin);
+        break;
+    case 6:
+        val = TCA_6.read1(pin);
+        break;
+    case 7:
+        val = TCA_7.read1(pin);
+        break;
+    default:
+        return false;
     }
     Serial.print(val);
     Serial.print('\t');
     return val;
 }
 
-bool TCAHandler::write(int TCA_num, int pin, bool value) {
-    switch (TCA_num) {
-        case 0: TCA_0.write1(pin, value); break;
-        case 1: TCA_1.write1(pin, value); break;
-        case 2: TCA_2.write1(pin, value); break;
-        case 3: TCA_3.write1(pin, value); break;
-        case 4: TCA_4.write1(pin, value); break;
-        case 5: TCA_5.write1(pin, value); break;
-        case 6: TCA_6.write1(pin, value); break;
-        case 7: TCA_7.write1(pin, value); break;
-        default: return false;
+bool TCAHandler::write(int TCA_num, int pin, bool value)
+{
+    switch (TCA_num)
+    {
+    case 0:
+        TCA_0.write1(pin, value);
+        break;
+    case 1:
+        TCA_1.write1(pin, value);
+        break;
+    case 2:
+        TCA_2.write1(pin, value);
+        break;
+    case 3:
+        TCA_3.write1(pin, value);
+        break;
+    case 4:
+        TCA_4.write1(pin, value);
+        break;
+    case 5:
+        TCA_5.write1(pin, value);
+        break;
+    case 6:
+        TCA_6.write1(pin, value);
+        break;
+    case 7:
+        TCA_7.write1(pin, value);
+        break;
+    default:
+        return false;
     }
     return true;
 }
 
-byte TCAHandler::getDeviceAddress(int TCA_num) {
+byte TCAHandler::getDeviceAddress(int TCA_num)
+{
     return TCA_address[TCA_num];
 }
 
 // Get the number of TCA found
-uint8_t TCAHandler::getNbTCA() {
+uint8_t TCAHandler::getNbTCA()
+{
     uint8_t Nb_TCA = 0;
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++)
+    {
         Wire.beginTransmission(TCA_address[i]);
         delay(50);
-        if (Wire.endTransmission() == 0) {
+        if (Wire.endTransmission() == 0)
+        {
             Nb_TCA++;
         }
     }
